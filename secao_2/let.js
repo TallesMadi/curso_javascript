0ae3869519f319beb1ae98fcec3696dad2733586
Talles <tallesmpinheirom@gmail.com>
2025-12-09 12:49:41 -0300

    let

--- secao_2/let.js
+++ secao_2/let.js
@@ -1,4 +1,13 @@
+// camelCase
+// case-sensitive
 let a = 1;
 let b = 2;
 let c = a + b;
 console.log(c);
+
+let primeiroNome; //Declara
+primeiroNome = 'Talles'; // Inicializa
+console.log('Meu nome é', primeiroNome);
+
+primeiroNome = 'Raquel';
+console.log('Meu nome é', primeiroNome);
