d2f74e2eb9a8290b78bf9adeaa95210f70efa115
Talles <tallesmpinheirom@gmail.com>
2025-12-09 15:26:32 -0300

    update

--- secao_2/alert_confirm_prompt/index.js
+++ secao_2/alert_confirm_prompt/index.js
@@ -1,3 +1,6 @@
 // alert('Mensagem de alert!');
 // confirm('Mensagem de confirm!');
-prompt('Mensagem de prompt!');
+const num1 = prompt('Digite um número!');
+const num2 = prompt('Digite outro número!');
+const soma = parseFloat(num1) + parseFloat(num2);
+alert(`Resultado da soma: ${soma}`);
