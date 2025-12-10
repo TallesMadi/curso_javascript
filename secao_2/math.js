70b0b498ff535de3618091f4ac8e19606407473e
Talles <tallesmpinheirom@gmail.com>
2025-12-10 09:34:55 -0300

    math

---
+++ secao_2/math.js
@@ -0,0 +1,12 @@
+let num1 = 9.5432;
+
+console.log(Math.floor(num1)); // arredondar para baixo
+console.log(Math.ceil(num1)); // arredondar para cima
+console.log(Math.round(num1)); // arrendonda para o mais próximo
+console.log(Math.max(-1, 50, 3, 320, 2));
+console.log(Math.min(-1, 50, 3, 320, 2));
+console.log(Math.random());
+console.log(Math.random() * (10 - 1) + 1);
+console.log(Math.PI);
+console.log(Math.pow(2, 10));
+console.log(Math.sqrt(144));
