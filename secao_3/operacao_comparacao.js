d2cd6f985e7938620dfa95a766b894ed00457cfc
Talles <tallesmpinheirom@gmail.com>
2025-12-11 11:59:13 -0300

    comparação

---
+++ secao_3/operacao_comparacao.js
@@ -0,0 +1,16 @@
+/**
+ * Operadores de comparação
+ * >
+ * >=
+ * <
+ * <=
+ * == igualdade
+ * === igualdade estrito
+ * != diferente (valor)
+ * !== diferente estrito (valor e tipo)
+ */
+
+console.log(5 == '5');
+console.log(5 === '5');
+console.log(5 != '5');
+console.log(5 !== '5');
