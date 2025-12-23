73831ca9320a84cb1a86f8ea5bd9845323f3bc35
Talles <tallesmpinheirom@gmail.com>
2025-12-23 15:27:13 -0300

    imc

---
+++ secao_3/timer/assets/js/main.js
@@ -0,0 +1,19 @@
+const resultado = document.querySelector('.resultado');
+const iniciar = document.querySelector('.iniciar');
+const pausar = document.querySelector('.pausar');
+const limpar = document.querySelector('.limpar');
+
+iniciar.addEventListener('click', function(e) {
+    e.preventDefault();
+    
+});
+
+pausar.addEventListener('click', function(e) {
+    e.preventDefault();
+    
+});
+
+zerar.addEventListener('click', function(e) {
+    e.preventDefault();
+    
+});
